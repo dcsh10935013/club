3a81b113ecacbfb239d725ed67f0500f3ce87a1a
20301 <10935013@dcsh.tp.ed.tw>
2022-06-03 14:54:31 +0800

    views import

--- log/views.py
+++ log/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.db.models import fields
+from django.shortcuts import render
+from django.views.generic import *
+from django.urls import reverse
+from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django import forms
 # Create your views here.
